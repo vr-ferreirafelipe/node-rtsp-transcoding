94d18f6bc2a78ef0e9bfad7ff3a6d6465adb7854
Felipe Ferreira <ferreira.felipe@vrsoft.com.br>
2024-03-13 09:43:22 -0300

    remove node_modules

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ Stream = require("node-rtsp-stream");
 stream = new Stream({
   name: "name",
   streamUrl:
-    "rtsp://admin:Mob$2024@192.168.1.108:554/cam/realmonitor?channel=1&subtype=0",
+    "rtsp://admin:Mob$2024@192.168.0.17:554/cam/realmonitor?channel=1&subtype=0",
   wsPort: 9999,
   ffmpegOptions: {
     "-stats": "",
